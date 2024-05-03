55c6a3b483c8ec5ed7694c941befce5f64d4221f
Noaman Saleem <22920@umt.edu.pk>
2024-05-03 16:46:21 +0500

    EJS Started

---
+++ 01-web-technologies-v1/05-NodeJS/05-EJS/routes/book-routes.js
@@ -0,0 +1,35 @@
+import express from "express";
+import {
+  createNewBook,
+  createNewBookPage,
+  deleteBook,
+  homePage,
+  readAllBooks,
+  readBook,
+  updateBook,
+} from "../controllers/book-controller.js";
+
+const router = express.Router();
+
+// index route
+router.route("/").get(homePage);
+
+// create book page route
+router.route("/books/new").get(createNewBookPage);
+
+// create book
+router.route("/books").post(createNewBook);
+
+// read all books
+router.route("/books").get(readAllBooks);
+
+// read specific book
+router.route("/books/:id").get(readBook);
+
+// update book
+router.route("/books/:id").patch(updateBook);
+
+// delete book
+router.route("/books/:id").delete(deleteBook);
+
+export default router;
